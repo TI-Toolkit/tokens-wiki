738bb6497d30b4ac70e10e4c0a929610c4577043
Adrien Bertrand <bertrand.adrien@gmail.com>
2023-02-24 00:15:03 +0100

    add ti-doc-parser and wikipages-gen

---
+++ parser.js
@@ -0,0 +1,312 @@
+"use strict"
+import * as fs from 'fs';
+import { JSDOM } from 'jsdom';
+import { XMLParser } from 'fast-xml-parser';
+import { parse as CSVParse } from 'csv-parse/sync';
+
+// guide xml input is done later, in a loop
+const tkXml= {}; // input
+const csv  = {}; // input
+const dict = {}; // input
+const json = {}; // output
+
+try {
+    const fileContents = fs.readFileSync('./adriweb_tokens.csv', 'utf8');
+    const records = CSVParse(fileContents, { columns: true, skip_empty_lines: true });
+    for (const tok of records) {
+        csv[tok['Readable Name']] = {
+            bytes: `0x${tok['Byte 1'].padStart(2, '0')}${tok['Byte 2'].length ? tok['Byte 2'].padStart(2, '0') : ''}`,
+            enName: tok['Readable Name'],
+            frName: tok['Readable Name (FR)'],
+            type: tok.Type?.toLowerCase() ?? '',
+            comment: tok.Comments,
+        };
+    }
+} catch (e) {
+    console.error(e);
+}
+
+try {
+    const fileContents = fs.readFileSync('./83PlusDictionary.xml', 'utf16le');
+    const {dictionary} = new XMLParser({ignoreAttributes: false, trimValues: false, attributeNamePrefix: "__"}).parse(fileContents);
+    for (const tok of dictionary["function"]) {
+        if (!tok.categories) {
+            continue;
+        }
+        if (typeof(tok.categories.category) === 'string' && tok.categories.category.startsWith('International')) {
+            continue;
+        }
+        tok.__name = tok.__name.replace('ñ', '⁻¹').replace('å', '►').replace('â', '𝗡').replace('ã', '𝐅').replace('Æ', 'Σ')
+                               .replace('ë', 'e').replace('ä', 'χ').replace('Ü', '²').replace('ü', '→').replace('Á', 'θ')
+                               .replace('û', 'ᴇ').replace('ë', 'e').replace('à', '𝑖');
+        if (tok.__name === 'sinh⁻¹') { tok.__name = 'sinh⁻¹('; } // sigh
+        dict[tok.__name] = tok;
+    }
+} catch (e) {
+    console.error(e);
+}
+
+try {
+    const fileContents = fs.readFileSync('./ti-toolkit_tokens_8X.xml', 'utf8');
+    const isArray = (name, jpath, isLeafNode, isAttribute) => {
+
+        const alwaysArray = [ "tokens.byte.token", "tokens.byte.byte.token",
+                              "tokens.byte.token.lang", "tokens.byte.byte.token.lang",
+                              "tokens.byte.token.lang.name", "tokens.byte.byte.token.lang.name" ];
+        if (alwaysArray.indexOf(jpath) !== -1) return true;
+    }
+    const {tokens} = new XMLParser({ignoreAttributes: false, trimValues: false, attributeNamePrefix: "__", isArray: isArray,
+                                    tagValueProcessor: (tagName, tagValue, jPath) => jPath.endsWith('.version') ? null : tagValue}
+                                  ).parse(fileContents);
+    if(tokens.byte) {
+        for (let tok of tokens.byte) {
+            if (tok.byte) {
+                for (const {token, __value} of tok.byte) {
+                    for (const tokAlt of token) {
+                        const bytes = `0x${tok.__value.substring(1)}${__value.substring(1)}`;
+                        tkXml[bytes] = {
+                            enName: tokAlt.lang[0].name[0],
+                            since: tokAlt.since ? { [tokAlt.since.model]: tokAlt.since.version } : undefined,
+                            until: tokAlt.until ? { [tokAlt.until.model]: tokAlt.until.version } : undefined,
+                        };
+                    }
+                }
+            } else {
+                for (const tokAlt of tok.token) {
+                    const bytes = `0x${tok.__value.substring(1)}`;
+                    tkXml[bytes] = {
+                        enName: tokAlt.lang[0].name[0],
+                        since: tokAlt.since ? { [tokAlt.since.model]: tokAlt.since.version } : undefined,
+                        until: tokAlt.until ? { [tokAlt.until.model]: tokAlt.until.version } : undefined,
+                    };
+                }
+            }
+        }
+    } else {
+        throw "wut";
+    }
+} catch (e) {
+    console.error(e);
+}
+
+for(let i = 0; i < 26; i++)
+{
+    const letter = (i+10).toString(36);
+    const folderPath = './e-guide_ref84plus_en/content/m_appxa/';
+
+    let fileContents;
+    try {
+        fileContents = fs.readFileSync(`${folderPath}/aa_appxa_${letter}.HTML`, 'utf8');
+    } catch (e) {
+        continue;
+    }
+
+    const tokens = (new JSDOM(fileContents)).window.document.body.querySelectorAll('table.TableStyle-RGFunction');
+    for (const token of tokens)
+    {
+        // some global replacements...
+        // ...ICOMsymbols
+        for (const [s, r] of Object.entries({ "3":"◄", "4":"►", "3 4":"◄►", "G":"Σ", "q":"θ", "c":"χ", "L":"-", "@":"Δ" })) {
+            token.innerHTML = token.innerHTML.replace(new RegExp(`<span class="Keys_ICOMSymbols"[^>]*>\\s*${s}\\s*</span>`, 'gi'), r);
+        }
+        // ...KeySymbols
+        for (const [s, r] of Object.entries({ "â":"ᴇ", "Ü":"𝐅", "Ù":"⌊", "ä":"*", "æ":"I%", "Ú":"𝗡", "!":"→" })) {
+            token.innerHTML = token.innerHTML.replace(new RegExp(`<span style="font-family: 'TI84KeySymbols'"[^>]*>${s}</span>`, 'gi'), r);
+        }
+        // ...superscripts etc.
+        token.innerHTML = token.innerHTML.replace(/<span class="Superscript" style="font-family: 'ICOM Symbols'[^>]*>L<\/span>\s*<span class="Superscript" [^>]*>1<\/span>\s*<span class="Function">\(<\/span>/g, '⁻¹(');
+        token.innerHTML = token.innerHTML.replace(/<span class="Superscript" style="font-family: 'ICOM Symbols'[^>]*>L<\/span>\s*<span class="Superscript" [^>]*>1<\/span>/g, '⁻¹');
+        token.innerHTML = token.innerHTML.replace(/<span class="Superscript" [^>]*>2<\/span>/g, '²');
+        // ...misc
+        token.innerHTML = token.innerHTML.replaceAll('‑', '-'); // really...?
+        token.innerHTML = token.innerHTML.replaceAll('► <span class="Function">F </span> ◄► <span class="Function">D</span>', '►F◄►D');
+        token.innerHTML = token.innerHTML.replaceAll('<span class="Function">r</span> <span class="Variable">e</span> <span class="Function">^</span> θ <span class="Variable">i</span>', 're^θ𝑖');
+        token.innerHTML = token.innerHTML.replaceAll('<span class="Function">6:R</span> ► <span class="Function">P</span> θ <span class="Function">(</span>', '6:R►Pθ(');
+
+        let name = null;
+        try {
+            const cmdNamePar = token.querySelector('thead > tr > th > p.CmdName');
+            const tmpFunc = cmdNamePar.querySelector('span.Function');
+            if (!tmpFunc) {
+                name = cmdNamePar.textContent.trim();
+            } else {
+                const cmdNameFromPar = cmdNamePar.textContent.trim().replace(/\)$/, '').replace(/\s+/g,' ').trim();
+                if (tmpFunc.textContent.trim().length < cmdNameFromPar.length) {
+                    name = cmdNameFromPar;
+                } else {
+                    name = tmpFunc.textContent.trim();
+                }
+            }
+
+            // Some special cases... :-|
+            name = name.replace(/(^:|\)$)/, '').replaceAll('/','⁄');
+            if (name === '1-VarStats')      { name = '1-Var Stats'; }
+            else if (name === '2-VarStats') { name = '2-Var Stats'; }
+            else if (name === 'Store →')    { name = '→'; }
+            else if (name === 'piecewise')  { name = 'piecewise('; }
+            else if (name === 'χ²GOF')      { name = 'χ²GOF-Test('; }
+            else if (name === 'summation Σ('){name = 'Σ('; }
+            else if (name === 'sinh⁻¹ (')   { name = 'sinh⁻¹('; }
+            else if (name === 'a+bi')       { name = 'a+b𝑖'; }
+            else if (name === 're^θi')      { name = 're^θ𝑖'; }
+            else if (name === '►F ◄►D')     { name = '►F◄►D'; }
+            else if (name === '►n⁄d ◄►Un⁄d'){ name = '►n⁄d◄►Un⁄d'; }
+            else if (name === 'i')          { name = '𝑖'; }
+            else if (name === 'SEQ(n')      { name = 'SEQ(𝒏)'; }
+            else if (name === 'DEC Answers'){ name = 'DEC'; }
+            else if (name === 'AUTO Answer'){ name = 'AUTO'; }
+        } catch (e) {
+            console.log(e)
+            continue;
+        }
+        console.log(`processing ${name}...`);
+
+        let type = "?";
+        let bytes = "?";
+        let categories = [];
+        let localizations = {};
+        let comment = undefined;
+
+        let commentRaw = token.querySelector('tbody p.SyntaxLine').innerHTML?.split('<br>');
+        if (commentRaw && commentRaw.length > 1) {
+            if (commentRaw[0].includes('<span')) {
+                commentRaw.shift();
+                comment = (new JSDOM(commentRaw)).window.document.body.textContent?.trim() ?? undefined;
+            } else {
+                // todo... comment and main thing locations can be swapped :/
+            }
+        }
+
+        const dictMatch = dict[name] ?? dict[`${name} `] ?? dict[`${name})`] ?? dict[`${name}) `] ?? dict[name.replace(/\($/,'')];
+        const csvMatch = csv[name] ?? csv[`${name} `] ?? csv[` ${name} `] ?? csv[`${name})`] ?? csv[`${name}) `] ?? csv[name.replace(/\($/,'')];
+
+        if (typeof(dictMatch) === 'object') {
+            if (name !== dictMatch.__name) {
+                console.warn(`   *****  adjusting  name from [${name}] to [${dictMatch.__name}]`);
+                name = dictMatch.__name;
+            }
+            if (!dictMatch.__token || dictMatch.__token.toLowerCase() !== name.toLowerCase()) {
+                type = dictMatch.__token ?? 'function';
+            }
+            bytes = dictMatch.__tag;
+            const cats = dictMatch.categories.category;
+            if (typeof(cats) === 'string') {
+                categories.push(cats)
+            } else {
+                categories = cats;
+            }
+        } else {
+            console.warn("   *****  no match found in dictionary")
+        }
+
+        if (typeof(csvMatch) === 'object') {
+            if (name !== csvMatch.enName) {
+                console.warn(`   *****  adjusting  name from [${name}] to [${csvMatch.enName}]`);
+                name = csvMatch.enName;
+            }
+            localizations.fr = csvMatch.frName;
+            if (!bytes || bytes === '?') {
+                bytes = csvMatch.bytes;
+            }
+            if ((!type || type === '?') && csvMatch.type) {
+                type = csvMatch.type;
+            }
+            if (!comment && csvMatch.comment) {
+                comment = csvMatch.comment;
+            }
+        } else {
+            console.warn("   *****  no match found in CSV")
+        }
+
+        const specialCategory = token.querySelector('thead > tr.Head-Header1 > th.HeadD-Column2-Header1 > p.MenuName')?.textContent.trim() ?? '';
+
+        const syntaxLine = token.querySelector('tbody p.SyntaxLine');
+        const wholeSyntaxLine = (syntaxLine?.textContent ?? '').replace(',[,','[,').replace(/ +/g,' ').trim();
+
+        let optionalSinceIdx = (wholeSyntaxLine.startsWith(`${name}[`) || wholeSyntaxLine.startsWith(`${name.trim()}[`)) ? 0 : null;
+        let _argIdx = 0;
+        let args = [...syntaxLine.childNodes]
+                    .filter(el => el.nodeType !== 8 /*COMMENT_NODE*/ && (!el.classList?.contains('Function') || el.textContent === ',' || el.getAttribute("style")?.includes('font-weight: normal')) && el.textContent.trim().length)
+                    .filter((el, idx) => {
+                        return (idx === 0 && el.textContent.includes(' ')) ? (el.textContent.split(' ')[0].trim() !== name.trim()) : true;
+                    })
+                    .map((el,idx,arr) => {
+                        const argStr = el.textContent.trim().replace(',[,','[,');
+                        if (optionalSinceIdx === null && /^\[,?$/.test(argStr)) {
+                            optionalSinceIdx = _argIdx
+                            if (optionalSinceIdx === 0 && name.endsWith('(')) {
+                                optionalSinceIdx = 1;
+                            }
+                            // console.log(`    optional args start now => ${optionalSinceIdx} ; ${wholeSyntaxLine}`)
+                        }
+                        if (el.textContent === ',' || el.classList?.contains('Variable')) {
+                            _argIdx += 1 + (argStr.replace(/(^,|,$)/g, '').match(/,/g) || []).length
+                        }
+                        return argStr;
+                    })
+                    .filter(el => el && el.textContent !== ',')
+                    .map((el) => [ el.replace(/(^,*|[\[\]\(\)]|,*$)/g, ''), '', false ])
+                    .map((el,idx,arr) => [ el[0], '', optionalSinceIdx !== null && idx >= optionalSinceIdx ])
+                    .filter(n => n && n[0].length && !/^[\[\]\(\)]$/.test(n[0]));
+
+        {
+            let newArgs = [];
+            for (const [idx, [argName, type, optional]] of Object.entries(args)) {
+                if (argName.includes(',')) {
+                    // console.warn(`    arg ${idx} wasn't split correctly: "${argName}". args was: ${JSON.stringify(args)}`);
+                    for (const [idx2, arg] of Object.entries(argName.split(','))) {
+                        newArgs.push([arg.replace(/^\(/, '').trim(), type, optional])
+                    }
+                } else {
+                    newArgs.push([argName, type, optional]);
+                }
+            }
+            args = newArgs;
+        }
+
+        // arg type determination...
+        args = args.filter(el => el && el[0].length);
+        for (const [idx, [argName]] of Object.entries(args)) {
+            if (/^listname\d/i.test(argName)) {
+                args[idx][1] = 'listName'
+            } else if (argName.includes('list')) {
+                args[idx][1] = 'list'
+            }
+            //console.log(idx, argName, arguments[idx][1])
+        }
+
+        const rawLocation = token.querySelector('tbody p.MenuName > span')?.parentElement;
+        const location = [ ...(rawLocation?.children ?? []) ]
+                         .map(el => el.textContent.trim())
+                         .filter(el => el)
+                         .map(el => el.replace(/^»$/, '[math]').replace(/^y$/, '[2nd]').replace(/^:$/, '[test]').replace(/^q$/, '[zoom]')
+                                      .replace(/^V$/, '[𝑖]').replace(/^>$/, '[matrix]').replace(/^…$/, '[stat]').replace(/^¼$/, '[prgm]')
+                                      .replace(/^=$/, '[distr]').replace(/^Œ$/, '[apps]').replace(/^\.$/, '[format]').replace(/^r$/, '[trace]')
+                                      .replace(/^9$/, '[list]').replace(/^˜$/, '[sin]').replace(/^\? $/, '[sin⁻¹]').replace(/^™$/, '[cos]')
+                                      .replace(/^@$/, '[cos⁻¹]').replace(/^š$/, '[tan]').replace(/^A$/, '[tan⁻¹]').replace(/^z$/, '[mode]')
+                                      .replace(/^L$/, '[mem]').replace(/^i$/, '𝑖').replace(/^-$/, '[tblset]').replace(/^;$/, '[angle]')
+                                      .replace(/^J$/, '[eˣ]').replace(/^D$/, '[ᴇᴇ]').replace(/^t$/, '[alpha]').replace(/^\^$/, '[F1]')
+                                      .replace(/^N$/, '[catalog]').replace(/^<$/, '[draw]').replace(/^,$/, '[stat plot]').replace(/^Z$/, '[ans]')
+                                      .replace(/^½$/, '[vars]').replace(/^æ$/, 'I%').replace(/^Ú$/, '𝗡').replace(/^ä$/, '*').replace(/^@$/, 'Δ'));
+
+        (json[name] ??= {
+            bytes: bytes,
+            type: type,
+            categories: categories,
+            syntaxes: [],
+            localizations: localizations,
+            since: tkXml[bytes]?.since,
+            until: tkXml[bytes]?.until,
+        }).syntaxes.push({
+            syntax: wholeSyntaxLine,
+            comment: comment,
+            arguments: args,
+            description: Array.from(token.querySelectorAll('tbody p.CmdDesc')).map(el => el.textContent.trim()).join("\n"),
+            inEditorOnly: (token.querySelector('tbody p.MenuName')?.textContent ?? '').includes('†'),
+            location: location.length > 1 ? location : [`[${name.replace(/\($/,'')}]`],
+            specialCategory: specialCategory.length ? specialCategory : undefined,
+        });
+    }
+}
+
+fs.writeFileSync('TI-84_Plus_CE_catalog-tokens.json', JSON.stringify(json, null, 2));
---
+++ wikipages-gen.js
@@ -0,0 +1,119 @@
+"use strict"
+import * as fs from 'fs';
+import sanitize from 'sanitize-filename';
+
+const tokens = JSON.parse(fs.readFileSync('./TI-84_Plus_CE_catalog-tokens.json', 'utf8'));
+
+for (const [tokName, token] of Object.entries(tokens)) {
+    let categories = '';
+    for (const category of token.categories) {
+        categories += `<li>${category}</li>`;
+    }
+
+    let page = `
+| Property      | Value |
+|---------------|-------|
+| Hex Value     | \`\$${token.bytes.substring(2)}\`|
+| Categories    | <ul>${categories}</ul> |
+
+# \`${tokName}\`
+`;
+
+    for (const info of token.syntaxes) {
+        page += `
+## Description
+${info.description}
+
+${info.comment ? ('<b>Comment</b>:' + token.comment + '\n') : ''}
+<b>Availability</b>: ${info.inEditorOnly ? 'Token only available from within the Basic editor.' : 'Token available everywhere.'}
+
+## Syntax
+\`${info.syntax}\`
+`;
+
+        if (info.arguments && info.arguments.length) {
+            page += `
+## Arguments
+<table>
+<tr><th>Name</th><th>Type</th><th>Optional</th></tr>
+`;
+            for (const arg of info.arguments) {
+                page += `
+<tr><td>${arg[0]}</td><td>${arg[1]}</td><td>${arg[2]}</td></tr>
+`;
+            }
+
+            page += `
+</table>
+`
+        }
+
+        if (info.description && info.description.length) {
+            page += `
+## Description
+${info.description}
+`;
+        }
+
+        if (info.location && info.location.length) {
+            page += `
+## Location
+`;
+            for (const [i, el] of info.location.entries()) {
+                if (el.startsWith('[')) {
+                    page += `<kbd>${el.substring(1, el.length-1)}</kbd>`;
+                } else {
+                    page += '`' + el + '`';
+                }
+                if (+i < info.location.length-1) {
+                    page += ', ';
+                }
+            }
+        }
+
+        if (info.specialCategory && info.specialCategory.length) {
+            page += `
+## Special Category
+${info.specialCategory}
+`;
+        }
+
+        page += `
+<hr>
+`;
+    }
+
+    page += `
+## Examples
+
+Explanation 1
+\`\`\`ti-basic
+code 1
+\`\`\`
+---
+Explanation 2
+\`\`\`ti-basic
+code 2
+\`\`\`
+
+## Error Conditions
+
+
+## Advanced Notes
+
+
+## History
+| Calculator | OS Version | Description |
+|------------|------------|-------------|
+`;
+    const sinceHTML = token.since ? `| <b>${Object.keys(token.since)[0]}</b> | ${Object.values(token.since)[0]} | Added` : '';
+    const untilHTML = token.until ? `| <b>${Object.keys(token.until)[0]}</b> | ${Object.values(token.until)[0]} | Removed` : '';
+    page += sinceHTML + untilHTML + '\n';
+
+    page += `
+## Related Commands
+
+    `;
+
+    fs.writeFileSync(`tokens/${sanitize(tokName)}.md`, page.trimStart());
+}
